a04d909915a7a8b0996482071bfbe1fc6ee1505e
CODEBECQ DAMIEN <damien.codebecq@lycee-jeanrostand.fr>
2024-10-10 10:47:01 +0200

    exercice5

--- Serie1/Exercice4.php
+++ Serie1/Exercice4.php
@@ -1,5 +1,5 @@
 <?php
-    $Argument1 = 'Antoine '; 
+    $Argument1 = 'Antoine'; 
     $Argument2 = 'Griezmann';
     function concatenation($Argument1,$Argument2){
         return $Argument1.$Argument2;
---
+++ Serie1/Exercice5.php
@@ -0,0 +1,10 @@
+<?php
+    $Argument1 = 'Ngolo';
+    $Argument2 = 'Kante';
+    function concatenationAvecEspace($Argument1,$Argument2){
+        return $Argument1.' '.$Argument2;
+
+    }
+    
+    echo concatenationAvecEspace($Argument1,$Argument2);
+?>
