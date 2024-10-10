a2d56393c6751adde892aab7daf399cab18cb247
CODEBECQ DAMIEN <damien.codebecq@lycee-jeanrostand.fr>
2024-10-10 10:49:43 +0200

    exercice6

---
+++ Serie1/Exercice6.php
@@ -0,0 +1,9 @@
+<?php
+    $Argument1 = 5 ;
+    $Argument2 = 5 ;
+    function somme($Argument1,$Argument2){
+            return $Argument1 + $Argument2;
+    }
+
+    echo somme($Argument1,$Argument2);
+?>
