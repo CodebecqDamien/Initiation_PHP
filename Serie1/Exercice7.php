15079f525acddf6b21bcdc837452d94e28402d9e
CODEBECQ DAMIEN <damien.codebecq@lycee-jeanrostand.fr>
2024-10-10 10:52:10 +0200

    exercice7

---
+++ Serie1/Exercice7.php
@@ -0,0 +1,9 @@
+<?php
+    $Argument1 = 5 ;
+    $Argument2 = 5 ;
+    function soustraction($Argument1,$Argument2){
+            return $Argument1 - $Argument2;
+    }
+
+    echo soustraction($Argument1,$Argument2);
+?>
