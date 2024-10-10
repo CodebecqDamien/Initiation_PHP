95dc3b5d8ab35a631920940bc3a6d3e2316d284c
CODEBECQ DAMIEN <damien.codebecq@lycee-jeanrostand.fr>
2024-10-10 10:53:21 +0200

    exercice8

---
+++ Serie1/Exercice8.php
@@ -0,0 +1,9 @@
+<?php
+    $Argument1 = 5 ;
+    $Argument2 = 5 ;
+    function multiplication($Argument1,$Argument2){
+            return $Argument1 * $Argument2;
+    }
+
+    echo multiplication($Argument1,$Argument2);
+?>
