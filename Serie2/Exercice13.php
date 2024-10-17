61e682c2c55f2150dd8f2b88b588824030b1b593
CodebecqDamien <144660038+CodebecqDamien@users.noreply.github.com>
2024-10-17 10:19:35 +0000

    Serie 2 - Exercice13

---
+++ Serie2/Exercice13.php
@@ -0,0 +1,11 @@
+<?php
+$countries = array(
+    "France" => "Paris",
+    "Allemagne" => "Berlin",
+    "Italie" => "Rome"
+);
+
+foreach ($countries as $country => $capital) {
+    echo "La capitale de $country est $capital.\n";
+}
+?>
