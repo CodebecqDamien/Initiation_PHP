f8c6ff3b674c813a3de9148e0a49d08df7580438
CodebecqDamien <144660038+CodebecqDamien@users.noreply.github.com>
2024-10-17 10:25:20 +0000

    Serie 2 - Exercice18

---
+++ Serie2/Exercice17.php
@@ -0,0 +1,14 @@
+<?php
+$pays_population = array(
+    'France' => 67595000,
+    'Suede' => 9998000,
+    'Suisse' => 8417000,
+    'Kosovo' => 1820631,
+    'Malte' => 434403,
+    'Mexique' => 122273500,
+    'Allemagne' => 82800000,
+);
+
+$nombre_elements = count($pays_population);
+echo "Le nombre d'éléments dans le tableau est : " . $nombre_elements;
+?>
