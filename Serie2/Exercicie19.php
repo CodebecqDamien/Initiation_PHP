4c091f059474175c93bfe96987f2880fabcda1bd
CodebecqDamien <144660038+CodebecqDamien@users.noreply.github.com>
2024-10-17 10:33:16 +0000

    Serie 2 - Exercice19

---
+++ Serie2/Exercicie19.php
@@ -0,0 +1,9 @@
+<?php
+    $personnes = array(
+        'Jean' => 16,
+        'Manuel' => 19,
+        'André' => 66
+    );
+
+    echo "L'âge de Manuel est " . $personnes['Manuel'] . " ans.";
+?>
